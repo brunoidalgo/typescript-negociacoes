30eb875ff2f08b016533c47ef8da81caffb09fbb
brunoidalgo <idalgob99@gmail.com>
2024-09-21 14:14:30 -0300

    Iniciando projeto

---
+++ app/app.ts
@@ -0,0 +1,9 @@
+import { NegociacaoController } from "./controllers/negociacao_controller.js";
+
+// Aqui estamos chamado a class e refereciando cada parâmetro do constructor.
+const controller = new NegociacaoController();
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    controller.adiciona();
+});
